b73008c2ebf65097960a4e998c949484e88cd137
SrinithiRajendran <srinithisri92@gmail.com>
2025-02-14 14:43:02 +0530

    removed scrollbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+// Add the custom-scrollbar class to the body element
+document.body.classList.add("custom-scrollbar");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
